0dd8302fed2d348cdd200466e6b6b3244a40e6d9
Kainechukwu <bismarckkaine@gmail.com>
2021-06-12 19:57:39 +0100

    adjusted responsiveness 1.0

--- src/landingPage.jsx
+++ src/landingPage.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react"
+import React, { useState } from "react"
 import TwitterIcon from '@material-ui/icons/Twitter';
 import { makeStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
@@ -33,7 +33,7 @@ function LandingPage() {
         setSignUpButtonHover(true);
     }
 
-    function signUpButtonOut(){
+    function signUpButtonOut() {
         setSignUpButtonHover(false);
     }
 
@@ -41,60 +41,80 @@ function LandingPage() {
         setButtonHover(true);
     }
 
-    function logInButtonOut(){
+    function logInButtonOut() {
         setButtonHover(false);
     }
 
     return (
-    <div className= "minWidth300">
-        <div className="displayFlex displayFlexColumn">
-            <div className="flex-left-page">
-                <div>
-                    <TwitterIcon viewBox="0 0 24 24" style={{ fontSize: 300, color: "white" }} />
+        <div className="minWidth300">
+            <div className="displayFlex displayFlexColumn"
+                style={{ backgroundColor: "white" }}
+            >
+                <div className="flex-left-page">
+                    <div>
+                        <TwitterIcon viewBox="0 0 24 24" style={{ fontSize: 300, color: "white" }} />
+
+                    </div>
+                </div>
+                <div className="page padding16px relative marginAutoWidth70">
+
+                    <TwitterIcon style={{ fontSize: 50, color: "#74cce6" }} />
+
+                    <Typography variant="h2"
+
+                        className={classes.typographyStyle1}
+                    >Happening now</Typography>
+
+                    <Typography
+                        variant="h4"
+                        className={classes.typographyStyle2}
+                    >Join Twitter Today.</Typography>
+
+
+                    <div className="flexColumn flexRow linkDiv">
+
+                        <Link to={{ pathname: "/signupReact" }}
+                            style={{ marginBottom: "25px", marginRight: "20px" }}
+                            className="buttonWidth textDecoration"
+                        >
+                            <div style={{ backgroundColor: signUpButtonHover ? "turquoise" : "rgba(29,161,242,1.00)" }}
+                                onMouseOver={signUpButtonOver}
+                                onMouseOut={signUpButtonOut}
+                                className="buttonDivs signUpButton">
+                                <span className="whiteText homeButtonText">Sign up</span>
+                            </div>
+                        </Link>
+
+
+                        <Link to={{ pathname: "/loginReact" }}
+                            className="buttonWidth textDecoration">
+                            <div style={{ backgroundColor: buttonHover ? "rgb(203 229 245)" : "white" }}
+                                onMouseOver={logInButtonOver}
+                                onMouseOut={logInButtonOut}
+                                className="buttonDivs loginButton">
+                                <span className="loginText homeButtonText"
+                                >
+                                    Login</span>
+                            </div>
+                        </Link>
+
+                    </div>
+
 
                 </div>
-            </div>
-            <div className="page padding16px relative marginAutoWidth70">
-
-                <TwitterIcon style={{ fontSize: 50, color: "#74cce6" }} />
-
-                <Typography variant="h2"
-
-                    className={classes.typographyStyle1}
-                >Happening now</Typography>
-
-                <Typography
-                    variant="h4"
-                    className={classes.typographyStyle2}
-                >Join Twitter Today.</Typography>
-
-
-                <div className="flexColumn flexRow linkDiv">
-
-                    <Link to={{ pathname: "/signupReact" }} style={{marginBottom: "25px", marginRight: "20px" }} className="buttonWidth textDecoration"> <div style={{backgroundColor: signUpButtonHover ? "turquoise" : "rgba(29,161,242,1.00)"}} onMouseOver={signUpButtonOver} onMouseOut={signUpButtonOut} className="buttonDivs signUpButton">
-                        <span className="whiteText homeButtonText">Sign up</span>
-                    </div></Link>
-
-
-                    <Link to={{ pathname: "/loginReact" }}  className="buttonWidth textDecoration"><div style={{backgroundColor: buttonHover ? "rgb(203 229 245)" : "white"}} onMouseOver={logInButtonOver} onMouseOut={logInButtonOut} className="buttonDivs loginButton">
-                        <span className="loginText homeButtonText">Login</span>
-                    </div></Link>
-
-                </div>
 
 
             </div>
 
-            
+            <div className="footer"
+                style={{ backgroundColor: "white" }}
+            >
+                <p> © {year} Twitter, Inc.</p>
+            </div>
         </div>
 
-        <div className="footer">
-               <p> © {year} Twitter, Inc.</p>
-            </div> 
-    </div>
-        
 
-        
+
 
     )
 
